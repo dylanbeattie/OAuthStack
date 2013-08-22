0f6f8a7570198ba561f9a476b0a24acce930a118
Dylan Beattie <dylan.beattie@spotlight.com>
2013-08-22 11:37:38 +0100

    Minor naming cleanup to fake client code.

--- src/OAuthStack.FakeServices/FakeOAuth2ClientStore.cs
+++ src/OAuthStack.FakeServices/FakeOAuth2ClientStore.cs
@@ -8,12 +8,12 @@ namespace OAuthStack.FakeServices {
     public class FakeOAuth2ClientStore : IOAuth2ClientStore {
         public const string CLIENT_IDENTIFIER = "fake_client";
         public const string CLIENT_SECRET = "fake_secret";
-        private static readonly FakeOAuth2Client FakeClient = new FakeOAuth2Client(CLIENT_IDENTIFIER, CLIENT_SECRET) {
+        private static readonly FakeOAuth2Client fakeClient = new FakeOAuth2Client(CLIENT_IDENTIFIER, CLIENT_SECRET) {
             ClientName = "OAuthStack Demo - Fake Client"
         };
 
         public IClientDescription GetClient(string clientIdentifier) {
-            if (clientIdentifier == CLIENT_IDENTIFIER) return (FakeClient);
+            if (clientIdentifier == CLIENT_IDENTIFIER) return (fakeClient);
             return (null);
         }
     }
